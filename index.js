142101524fb0170a70de3717494c5a4563f3fec6
Scott Mittman <sm2884@nau.edu>
2020-06-11 20:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 function deleteFromObjectByKey(object,key){
   let newObj=Object.assign({},object);
   delete newObj[key]
+  return newObj
 }
